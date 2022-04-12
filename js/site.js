969df625738c22f17006c41826bcde3cfa695809
nelsonleopold <nelsonleopold@gmail.com>
2022-04-12 09:46:24 -0700

    first attempt

--- js/site.js
+++ js/site.js
@@ -1,30 +1,48 @@
 // Step One, get the information (controller, accept requests)
+// todo
+// validate if the msg not equal to empty string
+// display in lowercase
+
 function getMessage() {
 
     let msg = document.getElementById("txtMessage").value;
 
-    displayMessage(msg);
-    
-    // this clears the input field after it's displayed
-    document.getElementById("txtMessage").value = '';
+    // validate that msg is not empty string
+    if (msg !== '') {
+        let message = reverseMessageA(msg);
+        displayMessage(message);
+        clrInputBox();
+    } else {
+        alert("Please enter some text, and I'll reverse it for you.");
+        clrInputBox();
+    }
 }
 
+function reverseMessageA(msg) {
+    let revMessage = "";
+
+    // decrementing for loop
+    // starts at end (array.length-1) of string/array and works to start (0)
+    for (let i = msg.length - 1; i >= 0; i--) {
+        let currentChar = msg[i];
+        revMessage += currentChar;
+    }
+
+    return revMessage;
+}
 
 
 // Final Step, display the information (view, displays stuff on screen)
 function displayMessage(message) {
 
-    // first get the ol element from the page
+    // first get the element from the page
     element = document.getElementById("results");
 
-    // next create a new li element
-    let item = document.createElement("li");
+    // print results to screen
+    element.innerHTML = message;
+}
 
-    // add classes to li element
-    item.classList.add("list-group-item");
-
-    // set the message for the li element
-    item.innerHTML = message;
-
-    element.appendChild(item);
+function clrInputBox() {
+    // this clears the input field after it's displayed
+    document.getElementById("txtMessage").value = '';
 }
